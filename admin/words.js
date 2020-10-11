14d64343b47c473fb976756e10868e9cfff7030c
Dominik Gerstner <coding@dg-services.de>
2020-10-11 12:10:03 -0700

    Initial Version

---
+++ admin/words.js
@@ -0,0 +1,69 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"somfy-api-connector-service adapter settings": {
+		"en": "Adapter settings for somfy-api-connector-service",
+		"de": "Adaptereinstellungen für somfy-api-connector-service",
+		"ru": "Настройки адаптера для somfy-api-connector-service",
+		"pt": "Configurações do adaptador para somfy-api-connector-service",
+		"nl": "Adapterinstellingen voor somfy-api-connector-service",
+		"fr": "Paramètres d'adaptateur pour somfy-api-connector-service",
+		"it": "Impostazioni dell'adattatore per somfy-api-connector-service",
+		"es": "Ajustes del adaptador para somfy-api-connector-service",
+		"pl": "Ustawienia adaptera dla somfy-api-connector-service",
+		"zh-cn": "somfy-api-connector-service的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	},
+	"user": {
+		"en": "User",
+		"de": "Benutzer"
+	},
+	"passwd": {
+		"en": "Password",
+		"de": "Passwort"
+	},
+	"appKey": {
+		"en": "Secret Application Key",
+		"de": "Geheimer Applikation Key"
+	},
+	"appSecret": {
+		"en": "Secret Application Password",
+		"de": "Geheimes Applikation Passwort"
+	},
+	"authKey": {
+		"en": "Authentication Key (provided by Husqvarna)",
+		"de": "Authentikation Schlüssel (von Husqvarna)"
+	},
+	"authState": {
+		"en": "Authentication State (provided by Husqvarna)",
+		"de": "Authentikation State (von Husqvarna)"
+	},
+	"loopInterval": {
+		"en": "Refresh Interval in minutes",
+		"de": "Aktualisierungsinterval in minuten"
+	}
+};
